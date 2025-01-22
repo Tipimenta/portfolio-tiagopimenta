758629e396bf83c4fb5e6a9353e39764eec05dc3
tipimenta <tiagopimenta.ata@gmail.com>
2025-01-22 17:32:11 -0300

    correcao

--- src/components/BackgroundSkills/BackgroundSkills.jsx
+++ src/components/BackgroundSkills/BackgroundSkills.jsx
@@ -1,5 +1,5 @@
 // import { motion } from "framer-motion";
-import styles from "./Backgroundskills.module.css";
+import styles from "./BackgroundSkills.module.css";
 
 const BackgroundSkills = () => {
  return (
